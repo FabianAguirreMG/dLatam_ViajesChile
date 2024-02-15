8dab3ed165082e6c5bc1ad41c2e1dc9c8305eb73
FabianAguirreMG <fabian@aguirre.digital>
2024-02-15 11:28:04 -0300

    Carga final. Se implementó tooltip, imagenes ok, alert y comentarios.

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,5 +1,5 @@
 $(function(){
-
+// Funcion que controla el smooth scroll
     $("a").click(function(event){
       if (this.hash !== "") {
         event.preventDefault();
@@ -8,14 +8,23 @@ $(function(){
   
         $("html, body").animate({
           scrollTop: $(anchor).offset().top
-        }, 800, function(){
+        }, 200, function(){
           window.location.hash = anchor;
         });
       }
     });
   
-    $(function () {
-      $('[data-toggle="tooltip"]').tooltip()
-    })
   
   });
+
+// funcion que habilita el uso de tooltips.
+$(function () {
+  $('[data-toggle="tooltip"]').tooltip()
+})
+
+//Funcion que lanza alert al enviar formulario
+$(document).ready(function () { 
+  $("#enviar").click(function () { 
+      alert("Formulario Enviado"); 
+  }); 
+}); 
